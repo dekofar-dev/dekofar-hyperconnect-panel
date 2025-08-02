259687fe2406809f332d40342e32a9b6e607b63f
dekofar-dev <dekofardeveloper@gmail.com>
2025-08-03 00:12:22 +0300

    feat: add commission tracking

--- src/app/modules/profile/overview/user-profile/user-profile.component.ts
+++ src/app/modules/profile/overview/user-profile/user-profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../auth/services/auth.service';
 import { UserModel } from '../../../auth/models/user.model';
+import { CommissionsService, Commission } from '../../../commissions/services/commissions.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -9,12 +10,17 @@ import { UserModel } from '../../../auth/models/user.model';
 })
 export class UserProfileComponent implements OnInit {
   user?: UserModel;
+  commissions: Commission[] = [];
+  commissionsLoading = false;
 
-  constructor(private auth: AuthService) {}
+  constructor(private auth: AuthService, private commissionsService: CommissionsService) {}
 
   ngOnInit(): void {
-    this.auth.currentUser$.subscribe(u => {
+    this.auth.currentUser$.subscribe((u) => {
       this.user = u ?? undefined;
+      if (u) {
+        this.loadCommissions();
+      }
     });
   }
 
@@ -26,5 +32,18 @@ export class UserProfileComponent implements OnInit {
     const name = this.user.fullName ?? '';
     const parts = name.trim().split(' ');
     return parts.slice(0, 2).map(p => p.charAt(0)).join('').toUpperCase();
+  }
+
+  private loadCommissions(): void {
+    this.commissionsLoading = true;
+    this.commissionsService.getByUser().subscribe({
+      next: (res) => {
+        this.commissions = res;
+        this.commissionsLoading = false;
+      },
+      error: () => {
+        this.commissionsLoading = false;
+      },
+    });
   }
 }
